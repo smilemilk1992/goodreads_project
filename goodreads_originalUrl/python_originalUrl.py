04307c18bc8620ae6dcaee965fc1becea6d6578d
jamesChen <702660780@qq.com>
2019-07-28 16:19:51 +0800

    add

--- goodreads_originalUrl/python_originalUrl.py
+++ goodreads_originalUrl/python_originalUrl.py
@@ -98,7 +98,7 @@ USER_AGENTS = [
 #                 executor.submit(getInfo, datas)
 
 def start():
-    x="64	https://www.goodreads.com/book/show/42130029	The Angry Dragon	Michael Gordon"
+    x="178	https://www.goodreads.com/book/show/492812	Angelina Ballerina	Katharine Holabird,Helen Craig"
     datas = x.split("\t")
     getInfo(datas)
 
